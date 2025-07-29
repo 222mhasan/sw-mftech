ce085a2eb1fc91e95c47d8905e8c254450e6dce1
Md Mehedi Hasan <1544364@mf.brac.net>
2025-07-29 16:35:03 +0600

    vercel file update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // This setting allows Vite dev server to serve index.html for unknown routes (during local dev)
+    fs: {
+      allow: ['.'],
+    },
+    historyApiFallback: true, // 👈 Fallback for client-side routing
+  }
 });
