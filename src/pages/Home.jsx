d701d05698486e4b9a9e5c3030e1966f21237096
Md Mehedi Hasan <1544364@mf.brac.net>
2025-09-01 10:59:10 +0600

    update 22

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import CircularSection from "../components/CircularSection";
 import Slider from "../components/Slider";
 import TOs from "../components/TOs";
+import HomeImage from "../../public/HomeImage/Animated Dashboards.json";
+import Lottie from "lottie-react";
 // import MonthlyReport from "./MonthlyReport";
 
 const Home = () => {
@@ -12,7 +14,18 @@ const Home = () => {
           <CircularSection></CircularSection>
         </div>
         <div className="col-span-2 border-gray-400 shadow-2xl bg-white rounded-md">
-          <h1 className="text-center my-2 font-semibold text-xl underline">History of BRAC Microfinance Technology</h1>
+          <h1 className="text-center my-2 font-semibold text-xl underline">
+            History of BRAC Microfinance Technology
+          </h1>
+          <div className="mx-auto w-fit" style={{ width: 300, height: 300 }}>
+            <Lottie animationData={HomeImage} loop={true} />
+          </div>
+          <p className="p-4 font-montserrat text-justify">
+            MF Technology basically is the Technology Unit of BRAC Microfinance
+            Programme. This Unit manage the all technology initiatives (like
+            Core Banking Module in ERP, Apps, Enterprise Applications), service
+            and supports for Microfinance Programme.
+          </p>
         </div>
         <div className="rounded-md border-gray-400 shadow-2xl">
           <TOs></TOs>
