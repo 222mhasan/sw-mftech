10c8b03c3319ca9e8e46132d4f5adb5ef17dbedb
Md Mehedi Hasan <1544364@mf.brac.net>
2025-11-27 18:02:40 +0600

    nov 27

--- src/components/EmergencyContacts.jsx
+++ src/components/EmergencyContacts.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const EmergencyContacts = () => {
+  const [centralLabs, setCentralLabs] = useState([]);
+  const [erpTeams, setErpTeams] = useState([]);
+
+  useEffect(() => {
+    fetch("/centralLab.json")
+      .then((res) => res.json())
+      .then((data) => setCentralLabs(data));
+  }, []);
+
+  useEffect(() => {
+    fetch("/erpTeam.json")
+      .then((res) => res.json())
+      .then((data) => setErpTeams(data));
+  }, []);
+
   return (
     <div>
-      <h1 className="text-center py-2 text-xl font-semibold bg-pink-500">
+      <h1 className="text-center py-2 mb-0.5 text-xl font-semibold bg-pink-500">
         Emergency Contacts
       </h1>
-      {/* Brac Net */}
-      <div className="bg-gray-200 border-2 text-center -space-y-1 rounded-md">
-        <h1 className="text-lg font-semibold ">BracNet Hotline</h1>
+      {/* Brac Net start*/}
+      <div className="bg-gray-300 border-2 text-center -space-y-1 rounded-md">
+        <h1 className="text-lg font-semibold text-black">BracNet Hotline</h1>
         <a
           href="tel:+8809677111222"
           className="text-blue-600 text-md font-bold underline hover:text-blue-800"
@@ -16,8 +31,9 @@ const EmergencyContacts = () => {
           +8809677111222
         </a>
       </div>
-      {/* Central lab */}
-      <div className="my-1">
+      {/* Brac Net end*/}
+      {/* Central lab start*/}
+      <div className="my-1 border-2 rounded-md">
         <div className="bg-base-100 border-base-300 collapse border">
           <input type="checkbox" className="peer" />
           <div className="collapse-title  text-black bg-gray-300 peer-checked:bg-secondary peer-checked:text-secondary-content">
@@ -26,32 +42,73 @@ const EmergencyContacts = () => {
               <h2>+</h2>
             </div>
           </div>
-          <div className="collapse-content  text-black space-y-1">
-           <div className="-space-y-1 text-center border-1 rounded-md mt-1">
-            <h2>Shovan Shahriar</h2>
-            <p>Lab Incharge</p>
-            <a
-              href="tel:+8801712345678" 
-                className="text-blue-600 text-md font-bold underline hover:text-blue-800"
-            >
-              +8801712345678
-            </a>
-           </div>
-           <div className="-space-y-1 text-center border-1 rounded-md">
-            <h2>Sharifuzzaman Khan Shawrav </h2>
-            <p>Security</p>
-            <a
-              href="tel:+880 1324-714677" 
-                className="text-blue-600 text-md font-bold underline hover:text-blue-800"
-            >
-              +880 1324-714677
-            </a>
-           </div>
+          <div className="collapse-content text-center text-black ">
+            {
+              /* Central lab contacts mapping */
+              centralLabs.map((lab) => (
+                <div
+                  className="card w-full bg-base-100bg-white text-black card-md shadow-sm rounded-none border-gray-300 border-b-1 my-1"
+                  key={lab.id}
+                >
+                  <div className="card-body -space-y-3 text-lg text-center">
+                    <h2 className=" font-semibold text-center">{lab.name}</h2>
+                    <h3>{lab.role}</h3>
+                    <a
+                      href={lab.phone}
+                      className="text-blue-600 text-md font-bold underline hover:text-blue-800"
+                    >
+                      {lab.phone.replace("tel:", "")}
+                    </a>
+                  </div>
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
-      {/* ERP Team */}
-      {/* <div>
+      {/* Central lab end*/}
+      {/* ERP Team  start*/}
+      <div className="my-1 border-2 rounded-md">
+        <div className="bg-base-100 border-base-300 collapse border">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title  text-black bg-gray-300 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <div className="flex justify-between text-black">
+              <h2 className="text-lg font-semibold ">ERP</h2>
+              <h2>+</h2>
+            </div>
+          </div>
+          <div className="collapse-content text-center text-black ">
+            {
+              /* ERP team contacts mapping */
+              erpTeams.map((erp) => (
+                <div
+                  className="card w-full bg-base-100bg-white text-black card-md shadow-sm rounded-none border-gray-300 border-b-1 my-1"
+                  key={erp.id}
+                >
+                  <div className="card-body -space-y-3 text-lg text-center">
+                    <h2 className=" font-semibold text-center">{erp.name}</h2>
+                    <h3>{erp.role}</h3>
+                    <a
+                      href={erp.phone}
+                      className="text-blue-600 text-md font-bold underline hover:text-blue-800"
+                    >
+                      {erp.phone.replace("tel:", "")}
+                    </a>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+      {/* ERP Team  end*/}
+    </div>
+  );
+};
+
+export default EmergencyContacts;
+
+/* <div>
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn m-1">
             Click
@@ -68,9 +125,4 @@ const EmergencyContacts = () => {
             </div>
           </div>
         </div>
-      </div> */}
-    </div>
-  );
-};
-
-export default EmergencyContacts;
+      </div> */
